06c75898132fd98ad30ad9a1e65019ed579989ce
MauroSorbello <sorbello.mauro.em.cn@gmail.com>
2026-03-12 10:36:29 -0300

    tokenTypes

--- Compilador/src/LexicalAnalizer/TokenTypes.java
+++ Compilador/src/LexicalAnalizer/TokenTypes.java
@@ -6,20 +6,228 @@ package LexicalAnalizer;
 
 public enum TokenTypes {
 
-    // Keywords
+//IDENTIFICADORES
+    /**
+     * Identificador de clase.
+     */
+    ID_CLASS,
+    /**
+     * Identificador de objeto.
+     */
+    ID_VAR,
 
-    KW_CLASS, KW_IMPL, KW_IF, KW_ELSE, KW_FOR, KW_WHILE,
+    //Clases predefinidas
+    /**
+     * Tipo de dato array.
+     */
+    ARRAY,
 
-    // id's
+    //CONSTANTES y VARIABLES
+    /**
+     * Tipo de dato cadena (string).
+     */
+    LITERAL_STR,
+    /**
+     * Tipo de dato entero (integer).
+     */
+    LITERAL_INT,
+    /**
+     * Tipo de dato booleano (boolean).
+     */
+    LITERAL_BOOL,
 
-    ID_CLASS, ID_VAR,
+    //PALABRAS CLAVE
+    //start no es palabra clave aunque sea el método inicializador.
 
-    // constantes
+    /**
+     * Palabra clave "class".
+     */
+    KW_CLASS,
+    /**
+     * Palabra clave "NIL".
+     */
+    KW_NIL,
+    /**
+     * Palabra clave "impl".
+     */
+    KW_IMPL,
+    /**
+     * Palabra clave "else".
+     */
+    KW_ELSE,
+    /**
+     * Palabra clave "if".
+     */
+    KW_IF,
+    /**
+     * Palabra clave "ret".
+     */
+    KW_RET,
+    /**
+     * Palabra clave "while".
+     */
+    KW_WHILE,
+    /**
+     * Palabra clave "true".
+     */
+    KW_TRUE,
+    /**
+     * Palabra clave "false".
+     */
+    KW_FALSE,
+    /**
+     * Palabra clave "new".
+     */
+    KW_NEW,
+    /**
+     * Palabra clave "fn".
+     */
+    KW_FN,
+    /**
+     * Palabra clave "st".
+     */
+    KW_ST,
+    /**
+     * Palabra clave "pub".
+     */
+    KW_PUB,
+    /**
+     * Palabra clave "self".
+     */
+    KW_SELF,
+    /**
+     * Palabra clave "div".
+     */
+    KW_DIV,
+    /**
+     * Palabra clave "for".
+     */
+    KW_FOR,
+    /**
+     * Palabra clave "in".
+     */
+    KW_IN,
+    /**
+     * Palabra clave "void". Aunque no esta en el inciso 6.4 palabras claves, si lo anuncia en el 6.3 como que sera tratado especialmente por tinyS
+     */
+    KW_VOID,
+    //TOKEN SIMBOLOS
+    /**
+     * Paréntesis izquierdo.
+     */
+    LEFT_PAREN,
+    /**
+     * Paréntesis derecho.
+     */
+    RIGHT_PAREN,
+    /**
+     * Coma.
+     */
+    COMMA,
+    /**
+     * Punto.
+     */
+    DOT,
+    /**
+     * Signo de suma.
+     */
+    PLUS,
+    /**
+     * Barra (división).
+     */
+    SLASH,
+    /**
+     * Signo de resta.
+     */
+    MINUS,
+    /**
+     * Llave izquierda.
+     */
+    LEFT_BRACE,
+    /**
+     * Llave derecha.
+     */
+    RIGHT_BRACE,
+    /**
+     * Punto y coma.
+     */
+    SEMICOLON,
+    /**
+     * Corchete izquierdo.
+     */
+    LEFT_BRACKET,
+    /**
+     * Corchete derecho.
+     */
+    RIGHT_BRACKET,
+    /**
+     * Asterisco (multiplicación).
+     */
+    OP_MULT,
+    /**
+     * Doble punto.
+     */
+    COLON,
 
-    LIT_INT, LIT_STRING,
+    //Tokens operaciones
+    // !   !=         =        ==         >           >=       <       <=        ++          --
+    /**
+     * Operador "not".
+     */
+    OP_NOT,
+    /**
+     * Operador "not equal".
+     */
+    OP_NOT_EQUAL,
+    /**
+     * Operador de asignación "equal".
+     */
+    OP_EQUAL,
+    /**
+     * Operador "equal equal".
+     */
+    OP_EQUAL_EQUAL,
+    /**
+     * Operador "greater than".
+     */
+    OP_GREATER,
+    /**
+     * Operador "greater than or equal".
+     */
+    OP_GREATER_EQUAL,
+    /**
+     * Operador "less than".
+     */
+    OP_LESS,
+    /**
+     * Operador "less than or equal".
+     */
+    OP_LESS_EQUAL,
+    /**
+     * Operador "plus plus" (incremento).
+     */
+    OP_PLUS_PLUS,
+    /**
+     * Operador "minus minus" (decremento).
+     */
+    OP_MINUS_MINUS,
+    /**
+     * Operador "and".
+     */
+    OP_AND,
+    /**
+     * Operador "or".
+     */
+    OP_OR,
+    /**
+     * Operador "percentage".
+     */
+    OP_PERCENTAGE,
 
-    // Operators
-
-    OP_ADD, OP_DIFF, OP_EQUALS, OP_ASSIGN, OP_MULTIPLY, OP_DIV
-
+    //Finalizar
+    /**
+     * Fin de archivo.
+     */
+    EOF
 }
+
